6b9d758a59d25b044a76cc6840d8cd26457b6d83
Eeedgar <edgar@datanirvana.org>
2020-07-30 08:19:21 +0200

    Added warning comment for the hard coded user agent

--- run.py
+++ run.py
@@ -63,6 +63,7 @@ def main():
 if __name__ == "__main__":
     facade(
         main,
+        # WARNING hard coded user agent
         user_agent='UNHCR_POPULATION',
         project_config_yaml=join("config", "project_configuration.yml"),
     )
