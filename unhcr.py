7c429edb5ffaea9bb45a643fe1ad33f4e4860c05
mcarans <rans@email.com>
2026-02-26 11:07:04 +1300

    Remove extra None

--- unhcr.py
+++ unhcr.py
@@ -225,7 +225,7 @@ def generate_dataset_and_showcase(
 
     if len(dataset.get_resources()) == 0:
         logger.error(f"{countryname}  has no data!")
-        return None, None, None
+        return None, None
     dataset.set_time_period(earliest_startdate, latest_enddate)
     showcase = Showcase(
         {
